4af67b19c793121737dbc92cebf9a4512ef66faa
Brian Ruff <briananthonyruff@gmail.com>
2022-04-29 10:40:45 -0400

    fix

---
+++ index.js
@@ -0,0 +1,72 @@
+(async () => {
+  function app() {
+    window.addEventListener("DOMContentLoaded", (e) => {
+      const result = document.querySelector("#results");
+      const numOne = document.querySelector("#numOne");
+      const numTwo = document.querySelector("#numTwo");
+      const numThree = document.querySelector("#numThree");
+      const submit = document.querySelector("#submit");
+      const resultFromNodeAndC = document.querySelector("#resultFromNodeAndC");
+
+      numOne.addEventListener("input", (_) => {
+        result.textContent = values();
+      });
+
+      numTwo.addEventListener("input", (_) => {
+        result.textContent = values();
+      });
+
+      numThree.addEventListener("input", (_) => {
+        result.textContent = values();
+      });
+
+      submit.addEventListener("click", (_) => {
+        const payload = currentResults();
+
+        fetch("https://be-multi-lang-app.azurewebsites.net/api/add", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            resultFromNodeAndC.textContent = data.result;
+          })
+          .catch((err) => {
+            console.log(err);
+            throw new Error(err);
+          });
+      });
+
+      function values() {
+        return `${inputValue(1)}, ${inputValue(2)}, ${inputValue(3)}`;
+      }
+
+      function currentResults() {
+        return result.textContent.split(", ");
+      }
+
+      function inputValue(num) {
+        if (num == 1) {
+          return numOne.value;
+        } else if (num == 2) {
+          return numTwo.value;
+        } else if (num == 3) {
+          return numThree.value;
+        } else {
+          throw new Error("invalid input number");
+        }
+      }
+    });
+  }
+
+  function runApp() {
+    app();
+  }
+
+  runApp();
+})();
